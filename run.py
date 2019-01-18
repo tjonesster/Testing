5327df211078e2077de6b13241973dc37750301c
Taylor Jones <tjonesster@gmail.com>
2019-01-18 02:04:41 -0600

    testing

---
+++ run.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+import os
+import sys
+
+'''
+r1filename="testcase_R1.fastq"
+r2filename="testcase_R2.fastq"
+'''
+r1filename="2834-MM-1_S1_L001_R1_001.fastq"
+r2filename="2834-MM-1_S1_L001_R2_001.fastq"
+
+fd1 = open(r1filename, 'r')
+fd2 = open(r2filename, 'r')
+
+counts = {}
+
+seen = {}
+
+fnamelist = []
+
+filenumber = 0 
+
+destinationFileIdList = []
+
+nestedList = []
+
+tmp = fd1.readline()
+
+# for i in range(0,10000000000000000000000000000):
+for i in range(0,100000000000000000000000000000):
+	#if i % 10000000 == 0:
+		#sys.stderr.write(str(i/10000000)+'\n')
+
+	tmp = fd1.readline().rstrip()
+
+	if tmp == "":
+		break
+
+	#print(tmp[0:10])
+
+	if tmp[0:10] in seen.keys():
+		destinationFileIdList.append(seen[tmp[0:10]])
+		counts[tmp[0:10]] += 1
+
+	else:
+		seen[tmp[0:10]]=filenumber
+		fnamelist.append(tmp[0:10])
+		counts[tmp[0:10]] = 1
+		destinationFileIdList.append(filenumber)
+		nestedList.append([])
+		nestedList[-1]=filenumber
+		filenumber+=1
+
+	fd1.readline()
+	fd1.readline()
+	fd1.readline()
+
+# skip this version in production
+
+fileDescriptors = []
+
+for thing in seen.keys():
+	fileDescriptors.append(-1)
+
+	if counts[thing] > 69363:
+		os.mkdir('./outputFastqs/'+thing)
+		fileDescriptors[seen[thing]] = open('./outputFastqs/'+thing+'/'+thing+".fastq", "w")
+
+	# print(counts[thing])
+
+trash = open("/dev/null", 'w')
+	
+# for basename in fnamelist:
+
+for i in range(0, 10000000000000000000000000):
+	#if fileDescriptors[destinationFileIdList[i]]:
+		fd = open(fnamelist[nestedList[i]], "a+")
+	#else:
+	#	fd = trash
+
+	tmp = fd.readline()
+
+	if tmp == "":
+		break
+
+	fd.write(tmp)
+	fd.write(fd.readline())
+	fd.write(fd.readline())
+	fd.write(fd.readline())
+
+	fd.close()
+
+	# fd3.close()
+
+
